bacae5648d15d4494fa25e4353a518731aa94bf0
Homi <bhome1@hotmail.com>
2026-04-29 20:59:25 +0700

    Forward single-thread worker output to the page

--- src/emscripten/public/engine-worker.js
+++ src/emscripten/public/engine-worker.js
@@ -3,16 +3,40 @@ importScripts("./stockfish-single.js");
 let enginePromise = null;
 let engine = null;
 let pendingCommands = [];
+const originalConsoleLog = console.log.bind(console);
+const originalConsoleError = console.error.bind(console);
+let consolePatched = false;
 
 function forward(line) {
   self.postMessage({ type: "line", line });
 }
 
+function patchConsole() {
+  if (consolePatched) {
+    return;
+  }
+
+  const relay = (writer, args) => {
+    const line = args
+      .map((value) => (typeof value === "string" ? value : String(value)))
+      .join(" ");
+    forward(line);
+    writer(...args);
+  };
+
+  console.log = (...args) => relay(originalConsoleLog, args);
+  console.error = (...args) => relay(originalConsoleError, args);
+  consolePatched = true;
+}
+
 async function initEngine() {
   if (!enginePromise) {
-    enginePromise = Stockfish().then((instance) => {
+    patchConsole();
+    enginePromise = Stockfish({ print: forward, printErr: forward }).then((instance) => {
       engine = instance;
-      engine.addMessageListener(forward);
+      if (typeof engine.addMessageListener === "function") {
+        engine.addMessageListener(forward);
+      }
 
       for (const command of pendingCommands) {
         engine.postMessage(command);
